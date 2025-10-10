d025f890781b22b416886d994bd1b240fe1a3648
Nedit <nedxha19@htl-shkoder.com>
2025-10-10 11:00:39 +0200

    I added the saveItems function to save them in localStorage

--- src/lib/utils/storage.js
+++ src/lib/utils/storage.js
@@ -1,2 +1,8 @@
 const LS_KEY = 'kanban_items';
 
+export const loadItems = () => {
+  const data = localStorage.getItem(LS_KEY);
+  return data ? JSON.parse(data) : [];
+};
+
+export const saveItems = (items) => localStorage.setItem(LS_KEY, JSON.stringify(items));
