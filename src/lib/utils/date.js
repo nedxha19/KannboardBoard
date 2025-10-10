d7978fa90d37db78a4d82d51f60fdc16e8040c4a
Nedit <nedxha19@htl-shkoder.com>
2025-10-10 11:43:49 +0200

    It will be used to show clear dates and warn if a task is late.

--- src/lib/utils/date.js
+++ src/lib/utils/date.js
@@ -1,1 +1,13 @@
+import { format, isPast, parseISO, isValid } from 'date-fns';
 
+export const formatDueDate = (isoDate) => {
+	if (!isoDate) return '';
+	const date = parseISO(isoDate);
+	return isValid(date) ? format(date, 'MMM d · HH:mm') : '';
+};
+
+export const isOverdue = (dueDate) => {
+	if (!dueDate) return false;
+	const date = parseISO(dueDate);
+	return isValid(date) && isPast(date);
+};
